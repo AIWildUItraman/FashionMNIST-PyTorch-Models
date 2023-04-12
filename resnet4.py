9cecbbdac3098d4e14051bb3971adf58a42e282d
AIWildUItraman <2833187860@qq.com>
2023-04-12 09:33:03 +0800

    dropout

--- resnet4.py
+++ resnet4.py
@@ -11,14 +11,15 @@ class Residual(nn.Module):
         self.p22 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1) 
         self.p31 = nn.Conv2d(inc, c3[0], kernel_size=1, stride=stride)
         self.p32 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
+        self.dropout = nn.Dropout2d(0.5)
         self.p41 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
         self.p42 = nn.Conv2d(inc, c4, kernel_size=1, stride=stride)
         self.con1x1 = nn.Conv2d(inc, outc, kernel_size=1, stride=stride) if use1x1 else None
         self.bn = nn.BatchNorm2d(outc)
     def forward(self, X):
         p1 = F.relu(self.p11(X))
-        p2 = F.relu(self.p22(F.relu(self.p21(X))))
-        p3 = F.relu(self.p32(F.relu(self.p31(X))))
+        p2 = F.relu(self.p22(self.dropout(F.relu(self.p21(X)))))
+        p3 = F.relu(self.p32(self.dropout(F.relu(self.p31(X)))))
         p4 = F.relu(self.p42(self.p41(X)))
         Y = self.bn(torch.cat((p1, p2, p3, p4), dim=1))
 
